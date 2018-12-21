847c8ef60e455b91ab0c2211d6bdc22975077096
Simon Berger <simberger@gmail.com>
2018-12-21 18:24:25 +0100

    more weirdness...

--- src/main.rs
+++ src/main.rs
@@ -10,35 +10,46 @@ pub mod scene_capnp {
 }
 
 mod test {
-    use std::fs::File;
-    // use std::io::Read;
     use memmap::MmapOptions;
+    use std::fs::File;
 
     use asset_capnp::{asset, asset_bundle, asset_pixel_data};
     use capnp::serialize;
     use capnp::Word;
 
-    pub fn with_reader<F: Fn(asset_bundle::Reader) -> capnp::Result<()>>(
-        f: F,
-    ) -> capnp::Result<()> {
-        let mut file = File::open("test.bin").unwrap();
-        let message_reader =
-            serialize::read_message(&mut file, ::capnp::message::ReaderOptions::new()).unwrap();
-        f(message_reader.get_root::<asset_bundle::Reader>()?)
+    pub enum CapnpMessageData {
+        Owned(capnp::message::Reader<capnp::serialize::OwnedSegments>),
+        Mapped(memmap::Mmap),
     }
 
-    pub fn with_reader_mmap<F: Fn(asset_bundle::Reader) -> capnp::Result<()>>( f: F,) -> capnp::Result<()> {
-        let file = File::open("test.bin").unwrap();
+    pub fn with_reader_owned<F: Fn(asset_bundle::Reader) -> capnp::Result<()>>(
+        f: F,
+    ) -> capnp::Result<()> {
+        with_reader(&read_owned(), f)
+    }
 
-        let mmap = unsafe { MmapOptions::new().map(&file).unwrap() };
+    pub fn with_reader_mmap<F: Fn(asset_bundle::Reader) -> capnp::Result<()>>(
+        f: F,
+    ) -> capnp::Result<()> {
+     	with_reader(&read_mmap(), f)
+    }
 
-        let message = serialize::read_message_from_words(
-            unsafe { Word::bytes_to_words(&mmap[..]) },
-            ::capnp::message::ReaderOptions::new(),
-        )
-        .unwrap();
-        f(message.get_root::<asset_bundle::Reader>().unwrap())
-        // Ok(())
+    pub fn with_reader<F: Fn(asset_bundle::Reader) -> capnp::Result<()>>(
+        data: &CapnpMessageData,
+        f: F,
+    ) -> capnp::Result<()> {
+        match data {
+            CapnpMessageData::Owned(reader) => f(reader.get_root::<asset_bundle::Reader>()?),
+            CapnpMessageData::Mapped(mmap) => {
+                let message = serialize::read_message_from_words(
+                    unsafe { Word::bytes_to_words(&mmap[..]) },
+                    ::capnp::message::ReaderOptions::new(),
+                )
+                .unwrap();
+
+                f(message.get_root::<asset_bundle::Reader>()?)
+            }
+        }
     }
 
     pub fn print_asset_bundle(asset_bundle: asset_bundle::Reader) -> capnp::Result<()> {
@@ -68,12 +79,29 @@ mod test {
         }
         Ok(())
     }
+
+    pub fn read_owned() -> CapnpMessageData {
+        let mut file = File::open("test.bin").unwrap();
+        let message_reader =
+            serialize::read_message(&mut file, ::capnp::message::ReaderOptions::new()).unwrap();
+        CapnpMessageData::Owned(message_reader)
+    }
+    pub fn read_mmap() -> CapnpMessageData {
+        let file = File::open("test.bin").unwrap();
+
+        let mmap = unsafe { MmapOptions::new().map(&file).unwrap() };
+        CapnpMessageData::Mapped(mmap)
+    }
+
 }
 fn main() {
     println!("Hello, world!");
 
     for _ in 0..100 {
-        //test::read_asset_bundle3().unwrap();
+        test::with_reader_owned(test::print_asset_bundle).unwrap();
+    }
+
+    for _ in 0..100 {
         test::with_reader_mmap(test::print_asset_bundle).unwrap();
     }
 }
